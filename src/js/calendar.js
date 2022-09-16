c343ae5a7b3c26f91c303bb4df544d9177a8c03b
sergisa <phebra@yandex.ru>
2022-09-16 20:26:03 +0300

    паттерны вынесены отдельно

--- src/js/calendar.js
+++ src/js/calendar.js
@@ -1,3 +1,6 @@
+let dayLinePattern = $(`<div class="dayLine row"></div>`);
+const headerPattern = $(`<div class="header row"></div>`);
+
 function generateDay(dayName, lessons) {
     let dayPattern = $(`<div class="day" data-day="${dayName}"></div>`)
     for (const lesson of lessons) {
@@ -7,15 +10,13 @@ function generateDay(dayName, lessons) {
 }
 
 function generateHeaderLine() {
-    const headerPattern = $(`<div class="header"></div>`);
     for (let day of calendarWeekDays) {
-        headerPattern.append(`<span>${day}</span>`)
+        headerPattern.append(`<span class="col-2">${day}</span>`)
     }
     return headerPattern
 }
 
 function generateDaysLine(shiftIndex) {
-    let dayLinePattern = $(`<div class="dayLine"></div>`);
     for (let i = (shiftIndex === undefined) ? 0 : shiftIndex; i < calendarWeekDays.length; i++) {
         dayLinePattern.append(generateDay(calendarWeekDays[i], [312]))
     }
@@ -23,11 +24,9 @@ function generateDaysLine(shiftIndex) {
 }
 
 function generateGrid(month) {
-    $(`<div class="month row"></div>`)
-        .append(generateHeaderLine())
+    $('#monthGrid').append(generateHeaderLine())
         .append(generateDaysLine(3))
         .append(generateDaysLine())
         .append(generateDaysLine())
         .append(generateDaysLine())
-        .appendTo($('#monthGrid'))
 }
