8c8377a7d0fb19680884b38d0cd7ae4ce8f6691f
sergisa <phebra@yandex.ru>
2022-10-11 18:50:02 +0300

    Добавление номера пары

--- src/js/calendar.js
+++ src/js/calendar.js
@@ -22,6 +22,7 @@ function generateDay(date, lessons, isMagistracy = false) {
         for (let i = 1; i <= (isMagistracy ? 7 : 5); i++) {
             const lesson = lessons.find((lesson) => lesson.Number === `${i} пара`);
             if (lesson) {
+                if (lesson.Coords.room.index.toLowerCase() === "спортивный зал") lesson.Coords.room.index = 'спорт. зал'
                 $lessonPattern.clone().html(`<span>${lesson.Coords.room.index}</span>`).attr("data-lesson-index", i).appendTo(dayView)
             } else {
                 dayView.append($lessonPattern.clone().attr("data-lesson-index", i).addClass('empty'))
@@ -73,6 +74,7 @@ function generateDaysLine(currentDate) {
 
 /**
  *
+ * @param container
  * @param month{number}
  */
 function generateGrid(container, month) {
