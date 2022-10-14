d41f57d079347de25d229317bf79d0babd47e797
sergisa <phebra@yandex.ru>
2022-10-14 15:09:54 +0300

    styling

--- views/lesson.blade.php
+++ views/lesson.blade.php
@@ -3,7 +3,7 @@
         <b class='fw-bold'>{{$lessonIndex}}. </b>{{$lessonSign}}
     </div>
     @if (isTeacherDefined())
-        <span class='lesson-description'>{{$groupsSign}} {{$courseSign}} {{$lessonAddress}} {{$type}}</span>
+        <span class='lesson-description'>{{$groupsSign}} {{$courseSign}} {{$type}} <br> {{$lessonAddress}}</span>
     @elseif(isGroupDefined())
         <span class='lesson-description'>{{$teacherSign}} {{$lessonAddress}} {{$type}}</span>
     @endif
