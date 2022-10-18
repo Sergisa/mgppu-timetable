7d355276fe4a81320911d509a1a79626dbc71bbf
Sergisa <phebra@yandex.ru>
2022-10-18 16:22:18 +0300

    Добавлено падение скрипта при отсутствии подключения

--- api.php
+++ api.php
@@ -1,13 +1,17 @@
 <?php
+header('Content-Type: application/json');
 include 'vendor/autoload.php';
 include 'functions.php';
 $route = explode('/', $_SERVER['PATH_INFO']);
 $response = null;
-if (end($route) === 'getProfessors') {
-    echo getProfessors()->toJson(JSON_PRETTY_PRINT | JSON_UNESCAPED_UNICODE | JSON_UNESCAPED_SLASHES | JSON_INVALID_UTF8_IGNORE);
-} else if (end($route) === 'getGroups') {
-    echo getGroups()->toJson(JSON_PRETTY_PRINT | JSON_UNESCAPED_UNICODE | JSON_UNESCAPED_SLASHES | JSON_INVALID_UTF8_IGNORE);
-} else if (end($route) === 'getTimetable') {
-    echo getPreparedTimetable()->values()->toJson(JSON_UNESCAPED_UNICODE | JSON_UNESCAPED_SLASHES | JSON_INVALID_UTF8_IGNORE);
+try {
+    if (end($route) === 'getProfessors') {
+        echo getProfessors()->toJson(JSON_PRETTY_PRINT | JSON_UNESCAPED_UNICODE | JSON_UNESCAPED_SLASHES | JSON_INVALID_UTF8_IGNORE);
+    } else if (end($route) === 'getGroups') {
+        echo getGroups()->toJson(JSON_PRETTY_PRINT | JSON_UNESCAPED_UNICODE | JSON_UNESCAPED_SLASHES | JSON_INVALID_UTF8_IGNORE);
+    } else if (end($route) === 'getTimetable') {
+        echo getPreparedTimetable()->values()->toJson(JSON_UNESCAPED_UNICODE | JSON_UNESCAPED_SLASHES | JSON_INVALID_UTF8_IGNORE);
+    }
+} catch (PDOException $e) {
+    die($e->getMessage());
 }
-
