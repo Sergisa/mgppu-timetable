58c8034cb79660b0e268f9be1c3ac366819326f8
sergisa <phebra@yandex.ru>
2022-10-19 14:35:57 +0300

    Настройка позиционирования и свёртки элементов.

--- views/header.blade.php
+++ views/header.blade.php
@@ -1,22 +1,25 @@
-<div class="month-title d-md-inline">
+<div class="month-title d-flex align-items-baseline mb-2">
     <a href="/" tabindex="1">
         <i class="bi bi-backspace fs-1"></i>
     </a>
-    <h1 class="fw-bolder d-inline">{{getMonths()[(int)getActiveMonth()]}}</h1>
-    <a href="?{{getNextMonthLink()}}" class="d-inline">
-        <i class="bi bi-arrow-right-square fs-1 float-end"></i>
+    <div>
+        <h1 class="fw-bolder d-inline m-0 ms-2">{{getMonths()[(int)getActiveMonth()]}}</h1>
+        @if (isTeacherDefined())
+            <p class="lead d-md-inline m-0 subtitle ms-2">
+                {{getTeacherById($_GET['professor'])}}
+            </p>
+        @endif
+        @if(isGroupDefined())
+            <p class="lead text-primary d-md-inline m-0 subtitle ms-2">
+                {{getGroupById($_GET['group'])}}
+            </p>
+        @endif
+    </div>
+    <a href="?{{getPreviousMonthLink()}}" class="d-inline ms-auto">
+        <i class="bi bi-arrow-left-square fs-1"></i>
     </a>
-    <a href="?{{getPreviousMonthLink()}}" class="d-inline">
-        <i class="bi bi-arrow-left-square fs-1 float-end"></i>
+    <a href="?{{getNextMonthLink()}}" class="d-inline">
+        <i class="bi bi-arrow-right-square fs-1"></i>
     </a>
 </div>
-@if (isTeacherDefined())
-    <p class="lead d-md-inline m-0 mb-md-3 subtitle">
-        {{getTeacherById($_GET['professor'])}}
-    </p>
-@endif
-@if(isGroupDefined())
-    <p class="lead text-primary d-md-inline m-0 mb-md-3 subtitle">
-        {{getGroupById($_GET['group'])}}
-    </p>
-@endif
+
