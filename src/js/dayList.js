d0a3d3d92b9adea94580b17659b2d8773e127d05
sergisa <phebra@yandex.ru>
2022-11-13 11:00:12 +0300

    Настроена прокрутка списка дней

--- src/js/dayList.js
+++ src/js/dayList.js
@@ -1,4 +1,4 @@
-const $dayListUl = $("#listDays>ul")
+const $dayListUl = $("#listDays")
 
 /**
  *
@@ -8,21 +8,25 @@ function scrollToDate(accentDate) {
     if (accentDate instanceof Date) {
         accentDate = accentDate.toLocaleDateString();
     }
-    let dayLiBlock = $dayListUl.find(`li[data-date='${accentDate}']`)
+    let $dayLiBlock = $dayListUl.find(`li[data-date='${accentDate}']`)
     const animateFn = 'easeOutCubic';
     const animateTime = 1000;
-    if (dayLiBlock.length > 0) {
-        if (breakPointEnabledDown(breakpointsUp.xs) && !breakPointEnabledDown(breakpointsUp.md)) {
+    if ($dayLiBlock.length > 0) {
+        if (breakPointEnabledUp(breakpointsUp.sm)) {
             $('html,body').animate({
-                scrollTop: dayLiBlock.position().top
+                scrollTop: $dayLiBlock.parent().prev('.date').position().top - $('.menu').outerHeight()
             }, animateTime, animateFn);
         } else {
             $dayListUl.animate({
-                scrollTop: dayLiBlock.position().top - $dayListUl.find('li:first').position().top
+                scrollTop:
+                    $dayLiBlock.parent().prev('.date').position().top -
+                    $dayListUl.find('li:first').position().top +
+                    $('.menu').outerHeight() -
+                    $('.date').outerHeight()
             }, animateTime, animateFn);
         }
         $dayListUl.find('.active').removeClass('active')
-        dayLiBlock.addClass('active')
+        $dayLiBlock.addClass('active')
     }
 }
 
