051516b1c63dc0bed1e22a42cc2bf52c11885fc6
Sergisa <phebra@yandex.ru>
2023-02-06 19:32:55 +0300

    Добавление переключателя месяца

--- views/roomsPage.blade.php
+++ views/roomsPage.blade.php
@@ -10,8 +10,13 @@
                     {{getBuildingById($_GET['building'])}}
                 </p>
             </div>
+            <a href="?{{getPreviousMonthLink()}}" class="d-inline ms-auto">
+                <i class="bi bi-arrow-left-square fs-1"></i>
+            </a>
+            <a href="?{{getNextMonthLink()}}" class="d-inline">
+                <i class="bi bi-arrow-right-square fs-1"></i>
+            </a>
         </div>
         <div class="room-list p-1 loading" id="roomsGrid"></div>
     </div>
-
 </div>
