86042b1baf323e06f92e3acc7325400fbe32edc2
Sergisa <phebra@yandex.ru>
2024-01-15 12:45:59 +0300

    FIX: Исправлен переход на страницу с комнатами

--- views/formLine.blade.php
+++ views/formLine.blade.php
@@ -1,8 +1,8 @@
 <div class="row align-content-between">
     <form action="{{ $link }}" class="mb-3 col-12 col-md-5 col-sm-6 col-xs-12 mx-auto">
-        <label for="{{ $identifier }}-select" class="text-light fs-4">{!! $slot !!}</label>
+        <label for="{{ $identifier }}-select" class="text-primary fs-4">{!! $slot !!}</label>
         <div class="mb-3 col-12 d-flex flex-column flex-md-row align-items-end align-items-md-center justify-content-between">
-            <select name="group" id="{{ $identifier }}-select" class="d-none"></select>
+            <select name="{{ $identifier }}" id="{{ $identifier }}-select" class="d-none"></select>
             <button class="mt-2 mt-md-0 btn btn-primary">Перейти</button>
         </div>
     </form>
