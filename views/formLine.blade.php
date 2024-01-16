7e924d44ece154a418385975503b5e394fcc4ceb
Sergisa <phebra@yandex.ru>
2024-01-16 23:22:15 +0300

    UPD: Добавлено использование Blade на главной странице

--- views/formLine.blade.php
+++ views/formLine.blade.php
@@ -1,5 +1,5 @@
 <div class="row align-content-between">
-    <form action="{{ $link }}" class="mb-3 col-12 col-md-5 col-sm-6 col-xs-12 mx-auto">
+    <form action="{{ $link }}.php" class="mb-3 col-12 col-md-5 col-sm-6 col-xs-12 mx-auto">
         <label for="{{ $identifier }}-select" class="text-primary fs-4">{!! $slot !!}</label>
         <div class="mb-3 col-12 d-flex flex-column flex-md-row align-items-end align-items-md-center justify-content-between">
             <select name="{{ $identifier }}" id="{{ $identifier }}-select" class="d-none"></select>
--- views/indexPage.blade.php
+++ views/indexPage.blade.php
@@ -1,9 +1,9 @@
-@component('formLine', ['link' => 'calendarView.php', 'identifier'=>'group'])
+@component('formLine', ['link' => 'calendarView', 'identifier'=>'group'])
     Расписание группы
 @endcomponent
-@component('formLine', ['link' => 'calendarView.php', 'identifier'=>'professor'])
+@component('formLine', ['link' => 'calendarView', 'identifier'=>'professor'])
     Расписание преподавателя
 @endcomponent
-@component('formLine', ['link' => 'rooms.php', 'identifier'=>'building'])
+@component('formLine', ['link' => 'rooms', 'identifier'=>'building'])
     Расписание аудиторий
 @endcomponent
